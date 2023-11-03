c1649da0b7cae910e574f5ee41a6542c0c7127de
Thalisson de Araujo Vieira <thalisson.vieira@>
2023-11-03 14:40:34 -0300

    primeiro commit

---
+++ src/test/java/com/db1/mentoria/MentoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db1.mentoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
